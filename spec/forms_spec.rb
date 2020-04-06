4b875f9b41f31de0454cda7630cbe52df613cd2a
Renata <renata.lima@cloudwalk.io>
2020-04-06 14:33:25 -0300

    ExemploAutomacao

---
+++ spec/forms_spec.rb
@@ -0,0 +1,20 @@
+
+
+describe 'forms' do
+
+    it 'login com sucesso' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'userId', with: 'stark'
+        fill_in 'password', with:'jarvis!'
+
+        click_button 'Login'
+
+        expect(find('#flash').visible?).to be true
+       
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+
+
+    end
+
+end
